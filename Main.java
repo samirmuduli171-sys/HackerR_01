0ee3ca15368a1010b951ffc52e509df0f49195a1
samirmuduli171-sys <smuduli171@gmail.com>
2026-09-11 12:15:49 +0530

    Hacker rank Question wrt odd and even

---
+++ Main.java
@@ -0,0 +1,40 @@
+package HackerR_01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        
+        
+        if( n>=2 & n<=5){
+            
+            if(n%2==0){
+                System.out.println("Not Weird");
+            }else{
+                System.out.println("Weird");
+            }
+                
+            
+        }
+        else if( n>=6 & n<=20 ){
+            if(n%2!=0){
+                System.out.println("Not Weird");
+            }else{
+                System.out.println("Weird");
+            }
+
+        }else if(n>20){
+            
+            if(n%2!=0){
+                System.out.println("Weird");
+            }else{
+                System.out.println("Not Weird");
+            }
+            
+            
+        }
+    }
+}
